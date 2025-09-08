c7336db64885f0da01f47e6cd589a226a8cbd55a
Nate VanBuskirk [SeriusSoft] <Nathan.VanBuskirk@SeriusSoft.com>
2025-09-08 00:02:49 -0500

    Merge and promote initial fly-thru of this extension from the rpi400 so I can edit on a sturdier machine

---
+++ switch.ts
@@ -0,0 +1,46 @@
+
+
+
+namespace switchCase {
+
+    /**
+     * Switch Block Container
+     */
+    //% block="switch $value"
+    //% blockId=switchcase_switch_block
+    //% group="Control"
+    //% weight=100
+    //% draggableParameters
+    export function switchBlock(value: any): void {
+        //this is jut a container; actual logic is in codegen
+        //Placeholder for CodeGen
+    }
+
+    /**
+     * Case Block Container
+     */
+    //% block="case $match"
+    //% blockId=switchcase_case_block
+    //% group="Control"
+    //% weight=90
+    //% draggableParameters
+    //% draggableStatement=true
+    export function caseBlock(match: any): void {
+        //Placeholder for CodeGen
+    }
+
+    /**
+     * Default-Case Block Container
+     */
+    //% block="default case"
+    //% blockId=switchcase_default_case_block
+    //% group="Control"
+    //% weight=80
+    //% draggableStatement=true
+    export function defaultCaseBlock(): void {
+        //Placeholder for CodeGen
+    }
+
+    
+}
+
